e81a698870c7e5e069447d48a58d9d115d5220ff
Arjun Patrawala <ajpat1234@gmail.com>
2019-02-06 18:13:15 -0800

    initial commit

---
+++ backend/lib/api.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const responses = require('./responses');
+
+
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+const api = require('./lib/api');
+
+app.use(express.json());
+
+app.options('*', cors());
+app.use(cors({
+    origin(origin, callback) {
+        callback(null, true);
+    }
+}));
+
+app.use('/api', api);
+app.use('/', express.static('public'));
+
+app.all('*', (req, res) => {
+    res.sendFile('./public/index.html');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on port ${process.env.PORT}`);
+});
